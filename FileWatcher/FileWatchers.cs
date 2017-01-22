5a6ef1ca2b8719f043f1a2441e2eb2313a1b3ed2
Александр <@ALEX>
2017-01-22 15:59:11 +0200

    Add project files.

---
+++ FileWatcher/FileWatchers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileWatcher
+{
+    class FileWatchers
+    {
+        public event Action ChangeFile;
+        public string Path;
+        public DateTime dt = DateTime.Now;
+        public object lockObject = new object();
+
+        public FileWatchers(string path)
+        {
+            Path = path;
+        }
+
+        public void Start()
+        {
+            Task t = new Task(ControlForChange);
+            t.Start();
+        }
+
+        public void ControlForChange()
+        {
+            lock (lockObject)
+            {
+                while (true)
+                {
+                    var writeTime = File.GetLastWriteTime(Path);
+                    if (writeTime > dt)
+                    {
+                        dt = writeTime;
+                        FileChange();
+                    }
+                }
+            }
+        }
+
+        protected void FileChange()
+        {
+            lock (lockObject)
+            {
+                Action handler = ChangeFile;
+                handler?.Invoke();
+            }
+        }
+    }
+}
---
+++ FileWatcher/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileWatcher
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            string path = "test.txt";
+            var myFile=File.Create(path);
+            myFile.Close();
+            var watcher = new FileWatchers(path);
+            
+            
+            watcher.ChangeFile += FileIsChange;
+            watcher.Start();
+            Console.ReadLine();
+        }
+
+        private static void FileIsChange()
+        {
+            string path = "test.txt";
+
+            if (File.ReadAllText(path) == "1")
+            {
+                File.WriteAllText(path, "0");
+                Thread.Sleep(10000);
+                Console.WriteLine("File content changed to 0 ten seconds ago ");
+            }
+            else
+            {                
+                Thread.Sleep(10000);
+                Console.WriteLine("File content is 0");
+            }
+        }
+    }
+}
